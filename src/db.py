4ac2cb192fd7db6f2c2ca967fb035e6dd3bfaad4
Aniket <surajani567@gmail.com>
2020-03-07 00:47:02 +0530

    proc and gunicorn

--- src/db.py
+++ src/db.py
@@ -3,8 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:daysunmon@127.0.0.1/test"
-SQLALCHEMY_DATABASE_URL = """postgres://eteunnmxstwmgc:94ec4a519ff2be01fc7260b7fa72da8848f6a02e234617c4168af5231337470e@ec2-18-235-97-230.compute-1.amazonaws.com:5432/d46rl9tcpluvr8"""
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:daysunmon@127.0.0.1/test"
+
 
 
 
