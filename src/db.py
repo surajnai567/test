1e18b2bfc493b250e44c47ffd29ef379bec0cc84
surajnai567 <53654029+surajnai567@users.noreply.github.com>
2020-03-11 20:32:10 +0530

    CHANGE IN DB FOR HERUKO

--- src/db.py
+++ src/db.py
@@ -1,11 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:daysunmon@127.0.0.1/test"
-
-
+#SQLALCHEMY_DATABASE_URL = "postgresql://postgres:daysunmon@127.0.0.1/test"
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
 
 
 engine = create_engine(
